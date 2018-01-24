4452eab4e94ee9c98fd9041c6211fd9157a91843
Kevin Ibanez <ibanez13keke@gmail.com>
2018-01-24 19:39:40 +0100

    Cases fonctionnel

---
+++ Assets/Script/S_CaseVide.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class S_CaseVide : MonoBehaviour {
+
+    public int colonne;
+    public int ligne;
+
+	// Use this for initialization
+	void Start ()
+    {
+      
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void SendValue(InputField input)
+    {
+        Debug.Log(input.text);
+    }
+}
---
+++ Assets/Script/S_GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_GameManager : MonoBehaviour {
+
+    private List<int> colonne1 = new List<int>();
+    private List<int> colonne2 = new List<int>();
+    private List<int> colonne3 = new List<int>();
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void AddANumber(int colonneSet, int ligneSet, int number)
+    {
+        
+    }
+}
